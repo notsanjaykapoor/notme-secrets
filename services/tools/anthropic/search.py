49c2941b81f9871b8d50b20636955a570703bc63
Sanjay Kapoor <notsanjaykapoor@gmail.com>
2025-09-06 15:29:48 -0500

    fix: update anth-search tool

--- services/tools/anthropic/search.py
+++ services/tools/anthropic/search.py
@@ -4,14 +4,14 @@ import anthropic
 import httpx
 import pydantic_ai
 
-url = "https://api.parallel.ai/v1beta/search"
+url = "https://api.anthropic.com/v1/messages"
 
 
 async def anth_search(
     context: pydantic_ai.RunContext[str],
     query: str,
     max_uses: int = 5,
-) -> list[dict]:
+) -> dict:
     """
     Search the web.
 
@@ -28,24 +28,31 @@ async def anth_search(
         "messages": [{"role": "user", "content": query}],
         "tools": [{"type": "web_search_20250305", "name": "web_search", "max_uses": max_uses}],
     }
+    headers = {
+        "x-api-key": f"{os.getenv('ANTHROPIC_API_KEY')}",
+        "anthropic-version": "2023-06-01",
+        "Content-Type": "application/json",
+    }
 
     print("anth_search params:")
     print(payload)
     print("")
 
-    async with anthropic.AsyncAnthropic(api_key=os.environ.get("ANTHROPIC_API_KEY")) as client:
-        results_list = []
+    async with httpx.AsyncClient() as client:
+        response_dict = {}
 
         try:
-            response = await client.messages.create(**payload)
-
-            breakpoint()  #
-            # response = await client.post(url, json=payload, headers=headers)
-            # response_json = response.json()
-            # results_list = response_json.get("results")
+            response = await client.post(url, json=payload, headers=headers, timeout=30)
+            response_dict = response.json()
         except httpx.HTTPStatusError as e:
             print(f"http error: {e.response.status_code} - {e.response.text}")
+            response_dict = {
+                "error": e.response.text,
+                "status_code": e.response.status_code,
+            }
+        except httpx.ReadTimeout as e:
+            print(f"http read timeout - url '{e.request.url}', error '{e}'")
         except httpx.RequestError as e:
             print(f"http request error - url '{e.request.url}', error '{e}'")
 
-    return results_list
+    return response_dict
